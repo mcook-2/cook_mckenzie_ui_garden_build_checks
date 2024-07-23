f8ea0327724eb114faa9f21ba3e647e836347a55
mcook-2 <153463932+mcook-2@users.noreply.github.com>
2024-07-23 16:34:17 -0500

    testing ci

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 const App = () => {
   const unusedVariable = 'I am not used. Delete me in the return to test lint';
-  return <div className="App">Hello World {unusedVariable} </div>;
+  const test = 'testing ci';
+  return (
+    <div className="App">
+      Hello World {unusedVariable} {test}{' '}
+    </div>
+  );
 };
 
 export default App;
