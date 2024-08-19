d726e7c465df5ec02f1c58d22f83a87b2a90b8a4
mcook-2 <153463932+mcook-2@users.noreply.github.com>
2024-08-18 21:51:19 -0500

    typo

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,5 @@
 export { default as Home } from './Home';
-export { default as Resources } from './Resources';
 export { default as About } from './About';
 export { default as Work } from './Work';
 export { default as Skills } from './Skills';
-export { default as DeveloperSetup } from './DeveloperSetup';
+
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8000
+    port: 8000,
+    host: true, // Expose the server on the network
+  },
+  preview: {
+    port: 8001, // Or any other port you prefer
   },
   build: {
     lib: {
@@ -20,6 +24,7 @@ export default defineConfig({
           'react-dom': 'ReactDOM',
         },
       },
+
     },
   },
 });
